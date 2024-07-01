5ce96773d29260e13dc8bc5e95a16a368ec27a37
Hugodonotexit <hugohk955.au@gmail.com>
2024-07-01 15:10:26 +0930

    OPT

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-
+#include <utility>
 #include "depen/var.h"
 #include "lexer/lexer.h"
 #include "lexer/readFile.h"
@@ -8,39 +8,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  // clock_t start_time, end_time;
-  // double cpu_time_used;
-
-  // start_time = clock();
   ReadFile readFile(argc, argv);
-  vector<string> input = readFile.getInput();
-  vector<vector<Token>> tokens;
-  Lexer lexer(input);
+  Lexer lexer(readFile.getInput());
   try {
-    tokens = lexer.tokenize();
-    /*Test only
-    for (const auto& token : tokens) {
-      for (const auto& token_line : token)
-      {
-        cout << "Token: " << token_line.value
-        << " Type: " << static_cast<int>(token_line.type) << endl;
-      }
-      cout << endl;
-    }
-    */
+    Parser_main parser(lexer.tokenize());
   } catch (const runtime_error& e) {
     cerr << "Error: " << e.what() << endl;
   }
-  try {
-    Parser_main parser(tokens);
-  } catch (const runtime_error& e) {
-    cerr << "Error: " << e.what() << endl;
-  }
-
-  // end_time = clock();
-  // cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-
-  // printf("Time taken: %f seconds\n", cpu_time_used);
-
   return 0;
 }
--- interpreter/parser/parser_main.cpp
+++ interpreter/parser/parser_main.cpp
@@ -1,6 +1,6 @@
 #include "parser.h"
 Parser_main::~Parser_main() {}
-Parser_main::Parser_main(vector<vector<Token>> &token) : MathSupport(token) {
+Parser_main::Parser_main(vector<vector<Token>> token) : MathSupport(token) {
   for (int line = 0; line < tokens.size(); line++) {
     for (int pos = 0; pos < tokens[line].size(); pos++) {
       switch (tokens[line][pos].type) {
@@ -16,15 +16,6 @@ Parser_main::Parser_main(vector<vector<Token>> &token) : MathSupport(token) {
   runFunction(mainIndex);
 }
 
-bool Parser_main::runFunction(int index) { return runFunction(index, -1, -1); }
-
-bool Parser_main::runFunction(int index, int _line_, int _pos_) {
-  vector<shared_ptr<Variable>> var;
-  return runFunction(index, -1, -1, var);
-}
-bool Parser_main::runFunction(int index, vector<shared_ptr<Variable>> var) {
-  return runFunction(index, -1, -1, var);
-}
 bool Parser_main::runFunction(int index, int _line_, int _pos_,
                               vector<shared_ptr<Variable>> var) {
   int varStartSize = variable.size();
@@ -388,10 +379,7 @@ bool Parser_main::runFunction(int index, int _line_, int _pos_,
   }
   return false;
 }
-int Parser_main::defVar(int &line, int &pos, int end_line, int end_pos) {
-  vector<shared_ptr<Variable>> pass;
-  return defVar(line, pos, end_line, end_pos, pass);
-}
+
 int Parser_main::defVar(int &line, int &pos, int end_line, int end_pos,
                         vector<shared_ptr<Variable>> pass) {
   shared_ptr<Variable> var;
@@ -619,109 +607,6 @@ int Parser_main::defVar(int &line, int &pos, int end_line, int end_pos,
   }
   variable.push_back(var);
   return variable.size() - 1;
-}
-
-void Parser_main::defFile(int line, int pos) {
-  File file(tokens[line][pos + 1].value);
-  int i = line;
-  int j = pos;
-  do {
-    j++;
-    if (j == tokens[i].size()) {
-      i++;
-      j = 0;
-      if (i == tokens.size()) {
-        i--;
-        j = tokens[i].size() - 1;
-        break;
-      }
-    }
-  } while (!(tokens[i][j].value == "`"));
-  file.setEndtPos(i, j);
-  scope.push_back(make_shared<File>(file));
-}
-
-template <typename T>
-int Parser_main::defScope(int line, int pos) {
-  int openbracket = 0;
-  int closebracket = 0;
-  int openRoundBracket = 0;
-  int closeRoundBracket = 0;
-  int i = line;
-  int j = pos;
-
-  optional<T> scoOpt;
-
-  if constexpr (is_same_v<T, Function>) {
-    scoOpt.emplace(tokens[line][pos + 1].value);
-  } else {
-    scoOpt.emplace();
-  }
-
-  T &sco = scoOpt.value();
-
-  while (openbracket != closebracket || openbracket == 0 || closebracket == 0) {
-    j++;
-    if (j >= tokens[i].size()) {
-      i++;
-      j = 0;
-      if (i >= tokens.size()) {
-        error(ERROR::BRACKET, i, j);
-      }
-    }
-    switch (tokens[i][j].type) {
-      case TokenType::L_RBACKET:
-        openRoundBracket++;
-        break;
-      case TokenType::R_RBACKET:
-        closeRoundBracket++;
-        break;
-      case TokenType::L_SBACKET:
-        openbracket++;
-        break;
-      case TokenType::R_SBACKET:
-        closebracket++;
-        break;
-    }
-    if (openRoundBracket == 1) {
-      sco.setConStartPos(i, j);
-      openRoundBracket++;
-    } else if (closeRoundBracket == 1) {
-      sco.setConEndPos(i, j);
-      closeRoundBracket++;
-    } else if (openbracket == 1) {
-      sco.setStartPos(i, j);
-      openbracket++;
-      closebracket++;
-    }
-  }
-  sco.setEndtPos(i, j);
-  if constexpr (is_same_v<T, Function>) {
-    for (int i = 0; i < tokens.size(); i++) {
-      for (int j = 0; j < tokens[i].size(); j++) {
-        if (tokens[i][j].type == TokenType::STRING) {
-          if (j > 0) {
-            if (tokens[i][j - 1].type != TokenType::DEF &&
-                sco.getname() == tokens[i][j].value) {
-              tokens[i][j].type = TokenType::FUNCTIONISED;
-              tokens[i][j].value = to_string(scope.size());
-            }
-          } else {
-            if (sco.getname() == tokens[i][j].value) {
-              tokens[i][j].type = TokenType::FUNCTIONISED;
-              tokens[i][j].value = to_string(scope.size());
-            }
-          }
-        }
-      }
-    }
-
-    if (sco.getname() == "main") {
-      mainIndex = scope.size();
-    }
-  }
-  scope.push_back(make_shared<T>(sco));
-  return (scope.size() - 1);
 };
 
 template <typename T>
